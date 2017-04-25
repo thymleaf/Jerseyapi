855b0601e4ce6a1431e5ad61e293d6827c39d1e1
thyme <thymejoe@gmail.com>
2017-04-25 14:22:23 +0800

    登录接口

--- src/main/java/com/gxtec/api/dao/UserDAO.java
+++ src/main/java/com/gxtec/api/dao/UserDAO.java
@@ -2,19 +2,35 @@ package com.gxtec.api.dao;
 
 import java.util.List;
 
-import org.hibernate.Query;
 import org.hibernate.Session;
+import org.hibernate.query.Query;
 
 import com.gxtec.api.bean.User;
 
+@SuppressWarnings("unchecked")
 public class UserDAO {
 
 	public List<User> getUsers() {
 
 		Session session = SessionUtil.getSession();
-		Query query = session.createQuery("from User");
-		List<User> users = query.list();
+		Query<User> query = session.createQuery("from User");
+		List<User> users = query.getResultList();
 		session.close();
 		return users;
+	}
+	
+	public User queryByName(String name, String password)
+	{
+		Session session = SessionUtil.getSession();
+		Query<User> query = session.createQuery(
+				"from User " +
+				"where name=:name and " + 
+				"password=:password")
+				.setParameter("name", name)
+				.setParameter("password", password);
+		System.out.println(query.getQueryString());
+		User user = query.uniqueResult();
+		session.close();
+		return user;
 	}
 }
--- src/main/java/com/gxtec/api/resource/UserResource.java
+++ src/main/java/com/gxtec/api/resource/UserResource.java
@@ -2,7 +2,10 @@ package com.gxtec.api.resource;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -14,6 +17,7 @@ import com.gxtec.api.bean.BaseResult;
 import com.gxtec.api.bean.User;
 import com.gxtec.api.dao.UserDAO;
 import com.gxtec.api.util.HttpCode;
+import com.gxtec.api.util.StringUtil;
 import com.topjavatutorial.dao.JacksonFilter;
 
 @Path("/users")
@@ -34,6 +38,7 @@ public class UserResource {
 			entity.setMsg("找不到用户");
 		}
 		
+		entity.setMsg("OK");
 		entity.setData(users);
 		
 		
@@ -53,4 +58,68 @@ public class UserResource {
 		
 		return Response.ok(result, MediaType.APPLICATION_JSON).build();
 	}
+	
+	
+	@POST
+	@Path("/login")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public Response login(@FormParam("username") String username, @FormParam("password") String password)
+	{
+		
+		BaseResult<User> entity = new BaseResult<>();
+		if(StringUtil.isEmpty(username) || StringUtil.isEmpty(password))
+		{
+			entity.setCode(HttpCode.UNAUTHORIZED);
+			entity.setMsg("用户名或密码不正确");
+			
+			return Response.ok(entity, MediaType.APPLICATION_JSON).build();
+		}
+		
+		
+		
+		UserDAO dao = new UserDAO();
+		User user = dao.queryByName(username, password);
+		if(user == null)
+		{
+			entity.setCode(HttpCode.UNAUTHORIZED);
+			entity.setMsg("用户名或密码不正确");
+			
+			return Response.ok(entity, MediaType.APPLICATION_JSON).build();
+		}
+		
+		entity.setCode(HttpCode.OK);
+		entity.setMsg("OK");
+		entity.setData(user);
+		
+		ObjectMapper mapper = new ObjectMapper();
+		JacksonFilter jacksonFilter = new JacksonFilter();
+	 
+//	    jacksonFilter.include(Employee.class, "id","name","age");
+	    jacksonFilter.filter(User.class, "id");
+	    mapper.setFilterProvider(jacksonFilter);  // 
+	    mapper.addMixIn(User.class, jacksonFilter.getClass()); // 
+	    String result = "";
+	    try {
+			result = mapper.writeValueAsString(entity);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		
+		return Response.ok(result, MediaType.APPLICATION_JSON).build();
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
