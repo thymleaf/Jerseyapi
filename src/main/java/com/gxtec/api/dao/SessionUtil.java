495fb5fdb3a980e36bc9a398d04c7c0a77ad5e52
thyme <thymejoe@gmail.com>
2017-04-25 10:17:35 +0800

    update hibernate from 4.3.10.Final to 5.2.10.Final

--- src/main/java/com/gxtec/api/dao/SessionUtil.java
+++ src/main/java/com/gxtec/api/dao/SessionUtil.java
@@ -2,23 +2,42 @@ package com.gxtec.api.dao;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
  
 public class SessionUtil {
     
-    private static SessionUtil instance=new SessionUtil();
+    private static SessionUtil instance = new SessionUtil();
     private SessionFactory sessionFactory;
     
     public static SessionUtil getInstance(){
             return instance;
     }
     
-    @SuppressWarnings("deprecation")
 	private SessionUtil(){
-        Configuration configuration = new Configuration();
-        configuration.configure("hibernate.cfg.xml");
-                
-        sessionFactory = configuration.buildSessionFactory();
+    	
+    	
+    	// A SessionFactory is set up once for an application!
+    	final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+    			.configure("hibernate.cfg.xml") // configures settings from hibernate.cfg.xml
+    			.build();
+    	try {
+    		sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
+    	}
+    	catch (Exception e) {
+    		// The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
+    		// so destroy it manually.
+    		System.out.println(e.getMessage());
+    		StandardServiceRegistryBuilder.destroy( registry );
+    	}
+    	
+    	
+    	
+//        Configuration configuration = new Configuration();
+//        configuration.configure("hibernate.cfg.xml");
+//                
+//        sessionFactory = configuration.buildSessionFactory();
     }
     
     public static Session getSession(){
